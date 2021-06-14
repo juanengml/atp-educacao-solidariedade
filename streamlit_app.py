178d67a0dcab95bbcd3fe70f671b82ab635eaeca
karamel <juan.marinho@viavarejo.com.br>
2021-06-14 12:31:48 -0300

    okok

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+from datetime import datetime as dt 
+
+def main():
+ st.title("Titulo app lascado")
+ st.header("Ola Mundo")
+ st.write(dt.now())
+
+if __name__ == "__main__":
+   main()
+
